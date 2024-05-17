610d41d9c1b09a360aa649a4783438523291c81c
Muhammad Ahmad <ahmadmuhammad.7700@gmail.com>
2024-05-17 04:05:46 -0700

    latest commits

---
+++ Controller/palmController.js
@@ -0,0 +1,32 @@
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MODEL_NAME = "models/chat-bison-001";
+const API_KEY = process.env.API_KEY;
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
+const palmController = async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    let messages = [{ content: `Provide detailed information on the following drug: ${prompt}. 
+    Include its effects, side effects, usage instructions, and alternate drugs.` }];
+
+    const result = await client.generateMessage({
+      model: MODEL_NAME,
+      prompt: { messages },
+    });
+
+    res.status(200).json({message: result[0].candidates[0].content});
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: `Some error Occurred ${error}` });
+  }
+};
+
+
+module.exports = { palmController };
